8043fd53de29efcd629a23ae0571040940bfc921
Glenn Thompson <gthompson@alaska.edu>
2012-04-04 11:36:41 -0800

    Updated rtexec and startup_tremor

--- startup_tremor.m
+++ startup_tremor.m
@@ -1,13 +1,14 @@
 warning off;
 matlab_antelope=getenv('MATLAB_ANTELOPE');
 addpath(genpath(matlab_antelope));
+addpath(genpath('~/src/gismotools/GISMO'));
 %startup_volcseis;
 tremor_home = getenv('TREMOR_HOME');
-if exist(tremor_home,'dir')
-	addpath(tremor_home);
-	chdir(tremor_home);
-end
+addpath(tremor_home);
+chdir(tremor_home);
 addpath(genpath('~/src/TreMoR/data/matlab'));
-debug(0);
+javaaddpath('/avort/modrun/lib/swarm.jar');
+javaaddpath('/avort/modrun/lib/usgs.jar');
+javaaddpath('/avort/modrun/lib/swarm-bin.jar');
 
 
