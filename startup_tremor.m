a3f66efa018044c8680a60aa64212308ac735a90
Glenn <gthompson@alaska.edu>
2013-04-29 15:41:22 -0800

    Updates to rtexec.pf and startup_tremor.m after reconfiguring setup on raven

--- startup_tremor.m
+++ startup_tremor.m
@@ -1,15 +1,10 @@
 warning off;
 matlab_antelope=getenv('MATLAB_ANTELOPE');
 addpath(genpath(matlab_antelope));
-addpath(genpath('~/src/gismotools_new/GISMO'));
-addpath('test');
-%startup_volcseis;
-tremor_home = getenv('TREMOR_HOME');
-addpath(tremor_home);
-chdir(tremor_home);
-addpath(genpath('~/src/TreMoR/data/matlab'));
-javaaddpath('/avort/modrun/lib/swarm.jar');
-javaaddpath('/avort/modrun/lib/usgs.jar');
-javaaddpath('/avort/modrun/lib/swarm-bin.jar');
+addpath(genpath('GISMO'));
+addpath(genpath('matlab'));
+javaaddpath('lib/swarm.jar');
+javaaddpath('lib/usgs.jar');
+javaaddpath('lib/swarm-bin.jar');
 
 
