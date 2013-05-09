b6f1755b9ae9a1fa0b5f4140acbfec0cb9d729c3
Glenn <gthompson@alaska.edu>
2013-05-08 16:17:29 -0800

    Modified exclude list based on today's data

--- data/pf/exclude_scnl.d
+++ data/pf/exclude_scnl.d
@@ -1,2 +1,121 @@
-# Nothing
-
+# STATION.CHANNEL.NETWORK.LOCATION
+#
+#--- PERMANENT REMOVALS
+SPNW.BHZ.AV
+IVS.EHZ.AV
+KAIC.EHZ.AV
+MSOM.EHZ.AV
+#
+#--- WINTER OUTAGES 2012/3
+# Commented out if they are excluded by automatic scheme anyway
+#--- Spurr ---#
+#CP2.EHZ.AV
+#SPCN.BHZ.AV
+SPBG.BHZ.AV
+SPCR.BHZ.AV
+#
+#--- Redoubt ---#
+#RDDF.BHZ.AV
+REF.EHZ.AV
+#--- Iliamna ---#
+#IVE.EHE.AV
+#OPT.EHZ.AV
+IVS.EHZ.AV
+ILI.EHZ.AV
+#
+#--- Augustine ---#
+#AUNW.EHZ.AV
+#AUSE.EHZ.AV
+AUH.EHZ.AV
+AUI.EHZ.AV
+AUE.EHZ.AV
+#
+#--- Fourpeaked ---#
+#FOSS.EHZ.AV
+#FOPK.EHZ.AV
+#CDD.EHZ.AV
+#
+#--- Katmai Region ---#
+#KAKN.BHZ.AV
+#KAPH.BHZ.AV
+KBM.EHZ.AV
+KAWH.EHZ.AV
+KABU.BHZ.AV
+ACH.EHZ.AV
+#
+#--- Aniakchak ---#
+ANON.EHZ.AV
+#
+#--- Veniaminof ---#
+VNNF.EHZ.AV
+#VNWF.EHZ.AV
+VNFG.EHZ.AV
+#VNSW.EHZ.AV
+#VNKR.EHZ.AV
+#
+#--- Pavlof/Dutton ---#
+#PV6.EHZ.AV
+#BLHA.EHZ.AV
+#PN7A.EHZ.AV
+DTN.EHZ.AV
+#
+#--- Shishaldin/Westdahl ---#
+SSLS.EHZ.AV
+ISLZ.EHZ.AV
+BRPK.EHZ.AV
+#WTUG.EHZ.AV
+#ISNN.EHZ.AV
+#
+#--- Makushin ---#
+#MGOD.EHZ.AV
+#MGOD.BHZ.AV
+#MAPS.BHZ.AV
+#
+#--- Okmok ---#
+#OKCF.EHZ.AV
+#OKID.EHZ.AV
+#OKCD.EHZ.AV
+#OKCB.EHZ.AV
+#OKFG.EHZ.AV
+#OKSO.EHZ.AV
+#OKCE.BHZ.AV
+#OKNC.BHZ.AV
+#
+#--- Korovin ---#
+# Cross feeding
+#KOKV.EHZ.AV is KONW.EHZ.AV
+#KOKV.EHN.AV is KOWE.EHZ.AV
+KOKV.EHZ.AV
+KOKV.EHN.AV
+KONE.EHZ.AV
+KOSE.EHZ.AV
+#
+#--- Great Sitkin ---#
+GSCK.EHZ.AV
+GSIG.SHZ.AV
+GSSP.EHZ.AV
+GSCK.EHZ.AV
+#
+#--- Tanaga ---#
+TACS.EHZ.AV
+#
+#--- Gareloi ---#
+GASW.EHZ.AV
+GAKI.EHZ.AV
+GANO.EHZ.AV
+GANE.EHZ.AV
+#
+#--- Semisopochnoi ---#
+CEPE.EHZ.AV
+#
+### Stations not working or too noisy, email from Natasha Ruppert 2013/05/01
+KTH.BHZ.AK
+VMT.BHZ.PS
+GLI.BHZ.AK
+FID.BHZ.AK
+HIN.BHZ.AK
+YAH.BHZ.AK
+#--- Regional subnets ---#
+HUR.SHZ.AK
+WAT3.BHZ.AK
+TRAP.EHZ.AK
