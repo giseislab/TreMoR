107db6cb6041dbe6be8be993dcd5134bb2c66dd5
Glenn Thompson <gthompson@alaska.edu>
2013-05-14 15:38:33 -0800

    Revert to power units for colorbar, instead of velocity because not confident of translation from spectral amplitude to velocity

--- data/matlab/toolbox/TreMoR/setup/colormaps/showRGBchannels.m
+++ data/matlab/toolbox/TreMoR/setup/colormaps/showRGBchannels.m
@@ -16,18 +16,20 @@ h=colorbar('EastOutside'); %generate colorbar
 cpos=get(h,'position'); %get position of colorbar
 set(h,'position',[cpos(1)+0.2 cpos(2:4)]); %shift colorbar
 ylabel1 = get(h,'ylabel'); 
-set(ylabel1,'fontWeight', 'bold','fontSize', 14, 'String',sprintf('Ground Velocity\n'));
+set(ylabel1,'fontWeight', 'bold','fontSize', 14, 'String',sprintf('Relative Spectral Power (dB)\n'));
+%set(ylabel1,'fontWeight', 'bold','fontSize', 14, 'String',sprintf('Ground Velocity\n'));
 ytick = get(h, 'YTick');
 dbtick = 10*ceil(decibelsrange(1)/10) : 10 : 10*floor(decibelsrange(2)/10);
 indextick = decibels2index(mymap, decibelsrange, dbtick);
-%set(h, 'YTick', indextick, 'YTickLabel',dbtick);
-dbticks = [0 10 20 30 40 50 60 70 80 90 100 110 120 130 140 150];
-velocityticklabels = {'1 fm/s'; '10 fm/s'; '0.1 pm/s'; '1 pm/s'; '10 pm/s'; '0.1 nm/s'; '1 nm/s'; '10 nm/s'; ...
-    sprintf('0.1 %cm/s', char(181)); sprintf('1 %cm/s',char(181)); sprintf('10 %cm/s',char(181)); ...
-    '0.1 mm/s'; '1 mm/s'; '1 cm/s'; '10 cm/s'; '1 m/s'};
-[sharedVals,idxs] = intersect(dbticks,dbtick);
-vlabels = velocityticklabels(idxs);
-set(h, 'fontSize', 12, 'YTick', indextick, 'YTickLabel',vlabels);
+set(h, 'YTick', indextick, 'YTickLabel',dbtick);
+
+%dbticks = [0 10 20 30 40 50 60 70 80 90 100 110 120 130 140 150];
+%velocityticklabels = {'1 fm/s'; '10 fm/s'; '0.1 pm/s'; '1 pm/s'; '10 pm/s'; '0.1 nm/s'; '1 nm/s'; '10 nm/s'; ...
+%    sprintf('0.1 %cm/s', char(181)); sprintf('1 %cm/s',char(181)); sprintf('10 %cm/s',char(181)); ...
+%    '0.1 mm/s'; '1 mm/s'; '1 cm/s'; '10 cm/s'; '1 m/s'};
+%[sharedVals,idxs] = intersect(dbticks,dbtick);
+%vlabels = velocityticklabels(idxs);
+%set(h, 'fontSize', 12, 'YTick', indextick, 'YTickLabel',vlabels);
 set(get(h, 'xlabel'), 'fontSize', 14, 'fontWeight', 'bold', 'String', sprintf('color scale\nis logarithmic'));
 
 function decibels=index2decibels(mymap,decibelsrange, x)
