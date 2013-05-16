ba63f43b8709cb6a03163e5a87df6bea0c96d90c
Glenn <gthompson@alaska.edu>
2013-05-15 21:27:16 -0800

    setup function will now select best 8 channels by taking into account several parameters including channel type, distance and percentage of available data

--- data/matlab/toolbox/TreMoR/setup/setup.m
+++ data/matlab/toolbox/TreMoR/setup/setup.m
@@ -1,15 +1,22 @@
-function setup()
-disp('Use this function to generate a MATLAB workspace for running the spectrograms from an Antelope-style parameter file (default: setup.pf). The MATLAB workspace, stored in a matfile, will contain a structures describing filesystem paths, spectrogram parameters, and a list of subnets and their coordinates, and for each subnet a list of station and channels, coordinates and calibration values. Since all of these variables change relatively infrequently, it is efficient to store them in a .mat file, rather than generate them from parameter/configuration files each time spectrograms are generated.')
-disp('There are two ways to generate the subnet and associated station-channel metadata. The first is to use the list of subnets in the setup parameter file, and then use the Antelope master stations database to choose the best station-channels using a channel mask and an exclude file listing bad station-channels. The second is to hand-create a configuration file containing these metadata (default: subnets.d).')
-choice = menu('Source for subnet definitions:', 'Generate from setup parameter file', 'Generate from existing (hand-edited) configuration file', 'Quit');
-switch choice
-	case 1, pf2matfile();
-	case 2, configurationfile2matfile();
-	case 3, return;
-	otherwise return;
+function setup(varargin)
+[runmode] = matlab_extensions.process_options(varargin, 'runmode', 'manual');
+if strcmp(runmode, 'manual')
+	disp('Use this function to generate a MATLAB workspace for running the spectrograms from an Antelope-style parameter file (default: setup.pf). The MATLAB workspace, stored in a matfile, will contain a structures describing filesystem paths, spectrogram parameters, and a list of subnets and their coordinates, and for each subnet a list of station and channels, coordinates and calibration values. Since all of these variables change relatively infrequently, it is efficient to store them in a .mat file, rather than generate them from parameter/configuration files each time spectrograms are generated.')
+	disp('There are two ways to generate the subnet and associated station-channel metadata. The first is to use the list of subnets in the setup parameter file, and then use the Antelope master stations database to choose the best station-channels using a channel mask and an exclude file listing bad station-channels. The second is to hand-create a configuration file containing these metadata (default: subnets.d).')
+	choice = menu('Source for subnet definitions:', 'Generate from setup parameter file', 'Generate from existing (hand-edited) configuration file', 'Quit');
+	switch choice
+		case 1, pf2matfile();
+		case 2, configurationfile2matfile();
+		case 3, return;
+		otherwise return;
+	end
+else
+	if strcmp(runmode, 'auto')
+		pf2matfile('runmode','auto');
+	end
 end
 
-function pf2matfile()
+function pf2matfile(varargin)
 % PF2MATFILE
 % Create a subnets.d and tremor_runtime.mat file from the subnets list in the setup.pf
 % 
@@ -20,17 +27,22 @@ function pf2matfile()
 
 MAX_CHANNELS_TO_FIND = 40;
 
-
-% Get user verification of filenames
-setupfile = dinput('Path of setup parameter file: ', 'pf/setup.pf');
+[runmode, setupfile, excludefile, subnetsdfile, matfile] = matlab_extensions.process_options(varargin, 'runmode', 'manual', 'setupfile', 'pf/setup.pf', 'excludefile', 'pf/exclude_scnl.d', 'subnetsdfile', 'pf/subnets.d', 'matfile', 'pf/tremor_runtime.mat');
+if strcmp(runmode, 'manual')
+	% Get user verification of filenames
+	setupfile = dinput('Path of setup parameter file: ', setupfile);
+	excludefile = dinput('Path of file containing station-channel-network combinations to exclude from spectrograms: ', excludefile);
+	subnetsdfile = dinput('Path of Earthworm-style configuration file to create: ', subnetsdfile);
+	matfile = dinput('Path of Matlab workspace (.mat) file to create: ', matfile);
+end
 if ~exist(setupfile, 'file')
 	error(sprintf('%s does not exist. Please create it first', setupfile));
 end
-excludefile = dinput('Path of file containing station-channel-network combinations to exclude from spectrograms: ', 'pf/exclude_scnl.d');
+
 if ~exist(excludefile, 'file')
 	error(sprintf('%s does not exist. Please create it first', excludefile));
 end
-subnetsdfile = dinput('Path of Earthworm-style configuration file to create: ', 'pf/subnets.d');
+
 if exist(subnetsdfile, 'file')
 	% archive current version of file with a timestamp reflecting last modification date
 	filemetadata = dir(subnetsdfile);
@@ -38,7 +50,7 @@ if exist(subnetsdfile, 'file')
     	system(sprintf('mv %s %s',subnetsdfile,oldsubnetsdfile));
 	warning(sprintf('%s already exists. The current version will be renamed to %s', subnetsdfile, oldsubnetsdfile));
 end
-matfile = dinput('Path of Matlab workspace (.mat) file to create: ', 'pf/tremor_runtime.mat');
+
 if exist(matfile, 'file')
 	% archive current version of file with a timestamp reflecting last modification date
 	filemetadata = dir(matfile);
@@ -62,10 +74,13 @@ disp('The subnets chosen are:');
 for c=1:length(subnetnames)
 	disp(sprintf('%d: %s',c, subnetnames{c}));
 end
-choice = dinput('Proceed ?', 'y');
-if ~strcmp(choice, 'y')
-	disp('Quitting')
-	return;
+
+if strcmp(runmode, 'manual')
+	choice = dinput('Proceed ?', 'y');
+	if ~strcmp(choice, 'y')
+		disp('Quitting')
+		return;
+	end
 end
 
 % open subnets.d file for writing
@@ -75,7 +90,7 @@ fout = fopen(subnetsdfile, 'w');
 laststation = 'ZZZZ';
 newsubnet_num = 0;
 for c=1:length(subnetnames)
-
+    clear thissubnet;
     thissubnet = subnets(c);
 
     % Find up to MAX_CHANNELS_TO_FIND scnls matching the channel mask within radius(km) of subnet latitude/longitude
@@ -90,75 +105,115 @@ for c=1:length(subnetnames)
 	end
     end
 
-    % Write out the subnet summary line
-    fprintf(fout, 'SUBNET\t%s\t%.4f\t%.4f\t%d\n',thissubnet.name, thissubnet.source.latitude, thissubnet.source.longitude, thissubnet.use);
-    fprintf('SUBNET\t%s\t%.4f\t%.4f\t%d\n',thissubnet.name, thissubnet.source.latitude, thissubnet.source.longitude, thissubnet.use);
 
-    % Write out scnl summary lines for this subnet
-    totalinuse = 0;
+
+    % See which stations are good
+    clear outstr;
+    clear percentagegot;
+    clear priority;
+    clear newstations;
+    clear use;
     for k=1:length(thissubnet.stations)
 
         fprintf('Processing %s.%s.%s\n',thissubnet.stations(k).name, thissubnet.stations(k).channel, get(thissubnet.stations(k).scnl, 'network'));
 	% If there is no calib value, don't even allow this as an option
-       	if isnan(thissubnet.stations(k).response.calib)
-        	fprintf(fout, '#nocalib\t%s.%s.%s\t%.4f\t%.4f\t%.2f\t%.4f\t%d\n',thissubnet.stations(k).name, thissubnet.stations(k).channel, get(thissubnet.stations(k).scnl, 'network'), thissubnet.stations(k).latitude, thissubnet.stations(k).longitude, thissubnet.stations(k).elev, NaN, 0);
-		continue;
+       	if isnan(thissubnet.stations(k).response.calib) || (thissubnet.stations(k).response.calib <=0.0)
+		outstr{k} = '#nocalib';
 	end
 
-	% If there are no waveform data within past 12 hours, don't even allow as an option
-	snum = utnow-0.5;
+	% If there are no waveform data within past 24 hours, don't even allow as an option
+	snum = utnow-1.1;
 	enum = utnow-0.1;
 	w = waveform_wrapper(thissubnet.stations(k).scnl, snum, enum, gismo_datasource);
 	if isempty(w)
-        	fprintf(fout, '#nodata\t%s.%s.%s\t%.4f\t%.4f\t%.2f\t%.4f\t%d\n',thissubnet.stations(k).name, thissubnet.stations(k).channel, get(thissubnet.stations(k).scnl, 'network'), thissubnet.stations(k).latitude, thissubnet.stations(k).longitude, thissubnet.stations(k).elev, thissubnet.stations(k).response.calib, 0);
+		percentagegot(k)=0.0;
+		outstr{k} = '#fail';
+	else
+		percentagegot(k) = waveform_soh(w, snum, enum);
+		if (percentagegot(k) == 0.0)
+			outstr{k} = '#nodata';
+		else
+			outstr{k} = 'scn';
+		end
+	end
+
+	% compute priority based on channel type, distance, and percentage of good data retrieved
+	switch thissubnet.stations(k).channel
+		case 'BHZ', priority(k) = 4;
+		case {'EHZ', 'SHZ'}, priority(k) = 3;
+		case {'BHN', 'BHE'}, priority(k) = 2;
+		case {'EHN', 'EHE', 'SHN', 'SHE', 'BDF'}, priority(k) = 1;
+		otherwise, priority(k) = 0;
+	end
+	priority(k) = percentagegot(k) * (priority(k) + (25.0 / thissubnet.stations(k).distance));
+	use(k) = 0;
+     end
+
+   % Decide which scn's to use based on 8 highest priority values, but without repeating a station
+   % make a copy that can be modified
+   clear pc;
+   pc = priority;
+   totalinuse = 0;
+   maxpc = 1;
+   while (maxpc > 0 && totalinuse < min([PARAMS.max_number_scnls length(thissubnet.stations)])),
+	% find the biggest priority
+        [maxpc, index] = max(pc);
+	if (maxpc <= 0)
 		continue;
 	end
 
-	% Check waveform soh
-	percentagegot = waveform_soh(w, snum, enum);
-	if percentagegot < 50
-        	fprintf(fout, '#%.1f\t%s.%s.%s\t%.4f\t%.4f\t%.2f\t%.4f\t%d\n',percentagegot, thissubnet.stations(k).name, thissubnet.stations(k).channel, get(thissubnet.stations(k).scnl, 'network'), thissubnet.stations(k).latitude, thissubnet.stations(k).longitude, thissubnet.stations(k).elev, thissubnet.stations(k).response.calib, 0);
-		continue;
-	end
-
-	% Should this scnl be used?
-        useit=0;
-        %if regexp(thissubnet.stations(k).channel, '[BES]HZ')  & (totalinuse < PARAMS.max_number_scnls) & (~excluded_scnl(thissubnet.stations(k).name, thissubnet.stations(k).channel) & ~strcmp(thissubnet.stations(k).name, laststation))
-        if regexp(thissubnet.stations(k).channel, PARAMS.channel_mask)  & (totalinuse < PARAMS.max_number_scnls) & (~excluded_scnl(excludefile, thissubnet.stations(k).name, thissubnet.stations(k).channel) & ~strcmp(thissubnet.stations(k).name, laststation))
-                useit = 1;
+	% switch it on for use if not excluded
+	if (~excluded_scnl(excludefile, thissubnet.stations(k).name, thissubnet.stations(k).channel))
+		use(index) =  1;
 		totalinuse = totalinuse + 1;
-		laststation = thissubnet.stations(k).name;
-		newstations(totalinuse) = thissubnet.stations(k);
-        end
+		newstations(totalinuse) = thissubnet.stations(index);
+		pc(index) = -999.0;
+	else
+		outstr{index} = strcat(outstr{index}, 'X');
+	end
 
-	% Write out scnl summary line for this scnl
-        fprintf(fout, 'scn\t%s.%s.%s\t%.4f\t%.4f\t%.2f\t%.4f\t%d\n',thissubnet.stations(k).name, thissubnet.stations(k).channel, get(thissubnet.stations(k).scnl, 'network'), thissubnet.stations(k).latitude, thissubnet.stations(k).longitude, thissubnet.stations(k).elev, thissubnet.stations(k).response.calib, useit);
+	% disallow any other channels for this station by setting pc to -ve
+	thissta = thissubnet.stations(index).name;
+	for k=1:length(thissubnet.stations)
+		thisothersta = thissubnet.stations(k).name;
+		if strcmp(thissta, thisothersta)
+			pc(k) = 0.0;
+		end
+	end
+   end
 
-    end
-    fprintf(fout, '\n\n'); % end of subnet
     % Exclude subnet with no stations, otherwise get a crash 8 lines below
-    if ~exist('newstations', 'var')
+    if (totalinuse == 0)
 	thissubnet.use = 0;
     end
+
     % Now we have a successfully fleshed-out metadata for this subnet, let's reset subnets accordingly
     if thissubnet.use
 	newsubnet_num = newsubnet_num + 1;
     	newsubnets(newsubnet_num) = thissubnet;
 	newsubnets(newsubnet_num).stations = newstations;
-
     end
-    % this variable might not exist, but can still clear it anyway
-    clear newstations;
+
+    % Write out the subnet summary line
+    fprintf(fout, 'SUBNET\t%s\t%.4f\t%.4f\t%d\n',thissubnet.name, thissubnet.source.latitude, thissubnet.source.longitude, thissubnet.use);
+    fprintf('SUBNET\t%s\t%.4f\t%.4f\t%d\n',thissubnet.name, thissubnet.source.latitude, thissubnet.source.longitude, thissubnet.use);
+  
+   % Write out scnl summary lines for this subnet
+   for k=1:length(thissubnet.stations)
+        fprintf(fout, '%s\t%s.%s.%s\t%.4f\t%.4f\t%.2f\t%.1f\t%.0f\t%.4f\t%d\n',outstr{k}, thissubnet.stations(k).name, thissubnet.stations(k).channel, get(thissubnet.stations(k).scnl, 'network'), thissubnet.stations(k).latitude, thissubnet.stations(k).longitude, thissubnet.stations(k).elev, thissubnet.stations(k).distance, priority(k), thissubnet.stations(k).response.calib, use(k));
+   end
+
+    fprintf(fout, '\n\n'); % end of subnet
 
 end 
 
 save2mat(matfile, newsubnets, paths, PARAMS);
 
 % update the subnets list for the spectrograms menu
-choice = dinput('Update the list of subnets used for the web interface menus? (y/n) ', 'n');
-if strcmp(choice, 'y')
-	update_web_subnets(subnets, paths);
-end
+%choice = dinput('Update the list of subnets used for the web interface menus? (y/n) ', 'n');
+%if strcmp(choice, 'y')
+	update_web_subnets(newsubnets, paths);
+%end
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 function configurationfile2matfile();
@@ -212,18 +267,21 @@ while 1,
             if strfind(tline, 'scn')
 		fprintf('%s\n', tline);
                 fields = regexp(tline, '\t', 'split');
-                if str2num(fields{7})==1 % use==1
+                if str2num(fields{9})==1 % use==1
                         cc = cc + 1;
                         scn = fields{2};
                         scnfields = regexp(scn, '\.', 'split');
                         subnets(c).stations(cc).name = scnfields{1};
                         subnets(c).stations(cc).channel = scnfields{2};
                         subnets(c).stations(cc).scnl = scnlobject(scnfields{1}, scnfields{2}, scnfields{3});
-                        subnets(c).stations(cc).distance = deg2km(distance(str2num(fields{3}), str2num(fields{4}), subnets(c).source.latitude, subnets(c).source.longitude));
+                        %subnets(c).stations(cc).distance = deg2km(distance(str2num(fields{3}), str2num(fields{4}), subnets(c).source.latitude, subnets(c).source.longitude));
+
                         subnets(c).stations(cc).latitude = str2num(fields{3});
                         subnets(c).stations(cc).longitude = str2num(fields{4});
                         subnets(c).stations(cc).elev = str2num(fields{5});
-                        subnets(c).stations(cc).response.calib = str2num(fields{6});
+                        subnets(c).stations(cc).distance = str2num(fields{6});
+                        subnets(c).stations(cc).priority = str2num(fields{7});
+                        subnets(c).stations(cc).response.calib = str2num(fields{8});
                 end
             end
         end
